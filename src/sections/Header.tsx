9a2e5b982824242b7d11a73db51e9f8b059cc8d8
Jorge Matos <jisapjisap@gmail.com>
2024-09-05 20:19:39 +0200

    init

---
+++ src/sections/Header.tsx
@@ -0,0 +1,32 @@
+
+
+
+
+export const HeaderSection = () => {
+
+  return (
+    <header className="sticky top-0 bg-zinc-900/50 backdrop-blur-lg">
+      <div className="container">
+        <div className="flex justify-between items-center h-24">
+          <div>
+            <img 
+              src="/assets/images/logo.svg"
+              alt="Blockhain logo"
+            />
+          </div>
+          <div>
+            <div className="size-10 relative">
+              <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+                <div className="w-5 h-0.5 bg-zinc-300 -translate-y-1"></div>   
+              </div>
+              <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+                <div className="w-5 h-0.5 bg-zinc-300 translate-y-1"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+  )
+}
