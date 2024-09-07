e8a92f6746f0fcd1bc199c0b8a4748cd0853a224
Jorge Matos <jisapjisap@gmail.com>
2024-09-07 17:29:06 +0200

    md:hero

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -3,15 +3,15 @@ import { CutCornerButton } from "../components/CutCornerButton"
 
 export const HeroSection = () => {
   return (
-    <section className="py-24 overflow-x-clip">
+    <section className="py-24 md:py-52 overflow-x-clip">
       <div className='container'>
         <p className="uppercase font-extrabold text-center text-zinc-500 tracking-wider">
           Introducing Blockforge
         </p>
-        <h1 className="font-heading font-black text-5xl text-center mt-4">
+        <h1 className="font-heading font-black text-5xl md:text-6xl text-center mt-4">
           The Future of Blockchain is Here.
         </h1>
-        <p className="text-center text-xl mt-6 text-zinc-400">
+        <p className="text-center text-xl md:text-2xl mt-6 text-zinc-400 max-w-xl mx-auto">
           Blockforge is pioonering smart contract integrity with cutting-edge data solutions.
         </p>
         <div className="flex justify-center mt-10">
@@ -19,16 +19,20 @@ export const HeroSection = () => {
             Get Started
           </CutCornerButton>
         </div>
-        <div className="inline-flex mt-24 relative z-0">
-          <img
-            src="/assets/images/icosahedron.png"
-            alt="Icosahedron"
-            className="absolute w-[calc(100%+100px)] max-w-none -z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 saturate-[10%] brightness-[4%] hue-rotate-[240deg]"
-          />
-          <img 
-            src="/assets/images/icosahedron.png" 
-            alt="Icosahedron" 
-          />
+
+        <div className="flex justify-center mt-24">
+          <div className="inline-flex relative z-0">
+            <img
+              src="/assets/images/icosahedron.png"
+              alt="Icosahedron"
+              className="absolute w-[calc(100%+100px)] max-w-none -z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 saturate-[10%] brightness-[4%] hue-rotate-[240deg]"
+            />
+            <img 
+              src="/assets/images/icosahedron.png" 
+              alt="Icosahedron" 
+              className="w-[500px]"
+            />
+          </div>
         </div>
       </div>
     </section>
