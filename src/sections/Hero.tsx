729c2b095f33792856427bedbcd0a701c123ca2c
Jorge Matos <jisapjisap@gmail.com>
2024-09-07 17:34:27 +0200

    lg:Hero

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -8,7 +8,7 @@ export const HeroSection = () => {
         <p className="uppercase font-extrabold text-center text-zinc-500 tracking-wider">
           Introducing Blockforge
         </p>
-        <h1 className="font-heading font-black text-5xl md:text-6xl text-center mt-4">
+        <h1 className="font-heading font-black text-5xl md:text-6xl lg:text-7xl text-center mt-4 max-w-3xl mx-auto">
           The Future of Blockchain is Here.
         </h1>
         <p className="text-center text-xl md:text-2xl mt-6 text-zinc-400 max-w-xl mx-auto">
