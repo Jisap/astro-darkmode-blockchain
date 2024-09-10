2ddb9c5913ef672b67e68c659ee7dad292ba7148
Jorge Matos <jisapjisap@gmail.com>
2024-09-10 17:28:34 +0200

    FeaturesCard structure

---
+++ src/sections/FeaturesCard.tsx
@@ -0,0 +1,34 @@
+
+
+
+
+export const FeaturesCard = () => {
+    return (
+      <section>
+        <div className="container">
+          <h2>Discover the future of blockchain with blockforce</h2>
+          <div>
+            <div>
+              <h3>Revolutionary Blockchain API</h3>
+              <p>
+                Effortessly integrate and manage blockchain data with our cutting-edge API, designed for seamless connectivity.
+              </p>
+              <div className="flex">
+                <button>Learn More</button>
+                <svg 
+                  xmlns="http://www.w3.org/2000/svg" 
+                  fill="none" 
+                  viewBox="0 0 24 24" 
+                  strokeWidth={1.5} 
+                  stroke="currentColor" 
+                  className="size-6"
+                >
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+                </svg>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    )
+  }
