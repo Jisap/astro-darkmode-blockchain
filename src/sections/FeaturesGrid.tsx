7d1806ffb181421ae36ce0031efacace2297985d
Jorge Matos <jisapjisap@gmail.com>
2024-09-12 15:38:35 +0200

    FeaturesGrid <li>

--- src/sections/FeaturesGrid.tsx
+++ src/sections/FeaturesGrid.tsx
@@ -11,25 +11,33 @@ const listItems = [
 
 export const FeaturesGrid = () => {
   return (
-    <section >
+    <section className="py-24">
       <div className="container">
-        <h2>Empowering the future of blockchain</h2>
-        <p>Blockforge provides robust ans secure infraestructure to support the next generation of descentralized applications.</p>
-        <ul>
-          {listItems.map((item) => (
-            <li key={item}>
-              <span>{item}</span>
-            </li>
-          ))}
-        </ul>
         <div>
-          <CutCornerButton>
-            Get Started
-          </CutCornerButton>
-          <TextButton>
-            Learn More
-          </TextButton>
+          <h2 className="font-heding font-black text-4xl">Empowering the future of blockchain</h2>
+          <p className="text-xl text-zinc-400 mt-8">Blockforge provides robust ans secure infraestructure to support the next generation of descentralized applications.</p>
+          <ul className="flex flex-col gap-8 mt-12">
+            {listItems.map((item) => (
+              <li key={item}>
+                <div className="inline-flex justify-center items-center size-8 outline outline-4 -outline-offset-4 outline-fuchsia-500/10 rounded-full ">
+                  <div className="size-1.5 bg-fuchsia-500 rounded-full"></div>
+                </div>
+                <span className="text-xl font-bold">{item}</span>
+              </li>
+            ))}
+          </ul>
+          <div>
+            <CutCornerButton>
+              Get Started
+            </CutCornerButton>
+            <TextButton>
+              Learn More
+            </TextButton>
+          </div>
         </div>
+
+        
+
       </div>
     </section>
   )
