cbbc322b058f2c27216c331772bb67d53e37b4bb
Jorge Matos <jisapjisap@gmail.com>
2024-09-12 15:41:03 +0200

    FeaturesGrid

--- src/sections/FeaturesGrid.tsx
+++ src/sections/FeaturesGrid.tsx
@@ -18,15 +18,15 @@ export const FeaturesGrid = () => {
           <p className="text-xl text-zinc-400 mt-8">Blockforge provides robust ans secure infraestructure to support the next generation of descentralized applications.</p>
           <ul className="flex flex-col gap-8 mt-12">
             {listItems.map((item) => (
-              <li key={item}>
-                <div className="inline-flex justify-center items-center size-8 outline outline-4 -outline-offset-4 outline-fuchsia-500/10 rounded-full ">
+              <li key={item} className="flex items-center gap-3">
+                <div className="inline-flex flex-shrink-0 justify-center items-center size-8 outline outline-4 -outline-offset-4 outline-fuchsia-500/10 rounded-full ">
                   <div className="size-1.5 bg-fuchsia-500 rounded-full"></div>
                 </div>
                 <span className="text-xl font-bold">{item}</span>
               </li>
             ))}
           </ul>
-          <div>
+          <div className="flex gap-8 mt-12">
             <CutCornerButton>
               Get Started
             </CutCornerButton>
