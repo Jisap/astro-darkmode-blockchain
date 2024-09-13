c913001eb8cd2a9a916f07b390b1c783a8233ba2
Jorge Matos <jisapjisap@gmail.com>
2024-09-13 18:39:37 +0200

    card

--- src/sections/LatestPost.tsx
+++ src/sections/LatestPost.tsx
@@ -4,20 +4,23 @@
 export const LatestPost = () => {
 
   return (
-    <section>
+    <section className="py-60">
       <div className="container">
-        <h2>Your portal to everything blockchain</h2>
-        <p>Keep up with newest trends, updates, and insights in the blockchain world, updated weekly.</p>
-        <div>
-          <div>
-            <div>Technology</div>
-            <h3>Regulatory Changllenges Facing Blockchain</h3>
-            <p>Understanding the regulatory landscape surrounding blockchain and what it means for the future of this technology.</p>
+        <h2 className="font-heading font-black text-4xl text-center">Your portal to everything blockchain</h2>
+        <p className="text-xl text-center text-zinc-400 mt-8">Keep up with newest trends, updates, and insights in the blockchain world, updated weekly.</p>
+        <div className="mt-16">
+          {[...new Array(4).fill(0).map((item, itemIndex) => (
             <div>
-              <button>Read more</button>
-              <div>Arrow</div>
+              <div>Technology</div>
+              <h3>Regulatory Changllenges Facing Blockchain</h3>
+              <p>Understanding the regulatory landscape surrounding blockchain and what it means for the future of this technology.</p>
+              <div>
+                <button>Read more</button>
+                <div>Arrow</div>
+              </div>
             </div>
-          </div>
+          ))]}
+          
         </div>
       </div>
     </section>
