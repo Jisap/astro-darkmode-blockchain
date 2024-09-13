693a67f5f46901a05c061c2e1cc0bf90262061a4
Jorge Matos <jisapjisap@gmail.com>
2024-09-13 19:23:05 +0200

    LatestPost

--- src/sections/LatestPost.tsx
+++ src/sections/LatestPost.tsx
@@ -1,3 +1,4 @@
+import { Card } from "../components/Card"
 
 
 
@@ -8,19 +9,23 @@ export const LatestPost = () => {
       <div className="container">
         <h2 className="font-heading font-black text-4xl text-center">Your portal to everything blockchain</h2>
         <p className="text-xl text-center text-zinc-400 mt-8">Keep up with newest trends, updates, and insights in the blockchain world, updated weekly.</p>
-        <div className="mt-16">
+        <div className="mt-16 flex flex-col gap-8">
           {[...new Array(4).fill(0).map((item, itemIndex) => (
-            <div>
-              <div>Technology</div>
-              <h3>Regulatory Changllenges Facing Blockchain</h3>
-              <p>Understanding the regulatory landscape surrounding blockchain and what it means for the future of this technology.</p>
-              <div>
-                <button>Read more</button>
-                <div>Arrow</div>
+            <Card
+              key={itemIndex}
+              buttonText="Read More"
+            >
+              <div className="px-3 py-1.5 uppercase font-heading font-extrabold tracking-wider text-xs bg-fuchsia-500/15 text-fuchsia-500 inline-flex rounded-full">
+                Technology
               </div>
-            </div>
+              <h3 className="font-heading font-black text-3xl mt-4">
+                Regulatory Changllenges Facing Blockchain
+              </h3>
+              <p className="text-lg text-zinc-400 mt-6">
+                Understanding the regulatory landscape surrounding blockchain and what it means for the future of this technology.
+              </p>
+            </Card>
           ))]}
-          
         </div>
       </div>
     </section>
