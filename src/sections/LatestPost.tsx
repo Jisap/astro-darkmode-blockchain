13f3ac854049635febe1d75ca3e36397986a6a4e
Jorge Matos <jisapjisap@gmail.com>
2024-09-13 18:18:30 +0200

    LatestPost structure

---
+++ src/sections/LatestPost.tsx
@@ -0,0 +1,25 @@
+
+
+
+export const LatestPost = () => {
+
+  return (
+    <section>
+      <div className="container">
+        <h2>Your portal to everything blockchain</h2>
+        <p>Keep up with newest trends, updates, and insights in the blockchain world, updated weekly.</p>
+        <div>
+          <div>
+            <div>Technology</div>
+            <h3>Regulatory Changllenges Facing Blockchain</h3>
+            <p>Understanding the regulatory landscape surrounding blockchain and what it means for the future of this technology.</p>
+            <div>
+              <button>Read more</button>
+              <div>Arrow</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
