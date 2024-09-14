28541600a958697779d0d752beb3815d12e2c850
Jorge Matos <jisapjisap@gmail.com>
2024-09-14 11:00:34 +0200

    blog

--- src/sections/LatestPost.tsx
+++ src/sections/LatestPost.tsx
@@ -5,28 +5,30 @@ import type { CollectionEntry } from "astro:content"
 
 export const LatestPost = (props: { latestPosts: CollectionEntry <'blog'>[]}) => {
 
+  const { latestPosts } = props;
+
   return (
     <section className="py-60">
       <div className="container">
         <h2 className="font-heading font-black text-4xl text-center">Your portal to everything blockchain</h2>
         <p className="text-xl text-center text-zinc-400 mt-8">Keep up with newest trends, updates, and insights in the blockchain world, updated weekly.</p>
         <div className="mt-16 flex flex-col gap-8">
-          {[...new Array(4).fill(0).map((item, itemIndex) => (
+          {latestPosts.map(({ data: { title, description, category }}, postIndex) => (
             <Card
-              key={itemIndex}
+              key={postIndex}
               buttonText="Read More"
             >
               <div className="px-3 py-1.5 uppercase font-heading font-extrabold tracking-wider text-xs bg-fuchsia-500/15 text-fuchsia-500 inline-flex rounded-full">
-                Technology
+                { category }
               </div>
               <h3 className="font-heading font-black text-3xl mt-4">
-                Regulatory Changllenges Facing Blockchain
+                { title }
               </h3>
               <p className="text-lg text-zinc-400 mt-6">
-                Understanding the regulatory landscape surrounding blockchain and what it means for the future of this technology.
+                { description}
               </p>
             </Card>
-          ))]}
+          ))}
         </div>
       </div>
     </section>
