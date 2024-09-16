1de92216808b1f8e038d024ccb2220f758fb2889
Jorge Matos <jisapjisap@gmail.com>
2024-09-16 13:45:39 +0200

    CTA

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,3 +1,4 @@
+import { Circle } from "../components/Circle";
 import { CutCornerButton } from "../components/CutCornerButton"
 import { Hexagon } from '../components/Hexagon';
 
@@ -13,6 +14,24 @@ export const CallToAction = () => {
           </div>
           <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
             <Hexagon className="size-[1100px]" />
+          </div>
+          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <Circle className="absolute left-0 -top-[400px]">
+              <img 
+                src="/assets/images/cuboid.png" 
+                alt="Cuboid" 
+                className="size-[140px]"
+              />
+            </Circle>
+          </div>
+          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <Circle className="absolute -left-[600px] -top-[70px]">
+              <img
+                src="/assets/images/cylinder.png"
+                alt="Cuboid"
+                className="size-[140px]"
+              />
+            </Circle>
           </div>
           <h2 className="font-heading font-black text-4xl md:text-5xl lg:text-6xl text-center">
             Ready to <span className="block">get started?</span>
