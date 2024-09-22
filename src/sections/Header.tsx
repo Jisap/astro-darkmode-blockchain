d2c472c283e8dd2be7d2c540acb7406003183d30
Jorge Matos <jisapjisap@gmail.com>
2024-09-22 14:40:05 +0200

    header finish

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CutCornerButton } from "../components/CutCornerButton"
 import { Hexagon } from "../components/Hexagon"
 import { twMerge } from "tailwind-merge";
@@ -27,6 +27,15 @@ const navLinks = [
 export const HeaderSection = () => {
 
   const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    if(isOpen) {
+      document.body.style.overflow = "hidden" // Prevent scrolling when menu is open
+    }else{
+      document.body.style.overflow = "auto"   // Allow scrolling when menu is closed
+    }
+
+  },[isOpen])
 
   return (
     <>
@@ -80,7 +89,7 @@ export const HeaderSection = () => {
             transition={{ duration: 0.5 }}
             className="fixed size-full top-0 left-0 z-30 bg-zinc-900"
           >
-            <div className="absolute inset-2 rounded-md bg-zinc-800 mt-24 z-0">
+            <div className="absolute inset-2 rounded-md bg-zinc-800 mt-24 md:mt-28 z-0">
               <div className="absolute top-1/2 left-full -translate-y-1/2 -translate-x-1/2 -z-10">
                 <Hexagon  size={700}/>
               </div>
@@ -88,7 +97,7 @@ export const HeaderSection = () => {
                 <Hexagon size={1100} />
               </div>
               <div className="h-full flex justify-center items-center">
-                <nav className="flex flex-col items-center gap-12">
+                <nav className="flex flex-col items-center gap-12 md:gap-16">
                   {navLinks.map(({ title, href }, index) => (
                     <motion.a 
                       initial={{ opacity: 0, y: 20 }}
@@ -101,7 +110,7 @@ export const HeaderSection = () => {
                       href={href} 
                       key={title}
                     >
-                      <span className="text-4xl font-heading font-black text-zinc-500 hover:text-zinc-300 transition duration-300">
+                      <span className="text-4xl md:text-5xl lg:text-6xl font-heading font-black text-zinc-500 hover:text-zinc-300 transition duration-300">
                         {title}
                       </span>
                     </motion.a>
